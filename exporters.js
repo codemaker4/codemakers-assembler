c6145140d5c132cbf1ada71946b8f2c5b3e9c763
CodeMaker4 <codemaker4@gmail.com>
2024-01-10 10:35:10 +0100

    refactor seperate download function

--- exporters.js
+++ exporters.js
@@ -1,13 +1,8 @@
-function exportVincelingMem() {
-    let data = compiler.export();
-    if (data === undefined || data.length == 0) {
-        alert("Export failed")
-        return;
-    }
-
+// https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
+function download(filename, text) {
     var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(data)));
-    element.setAttribute('download', 'data.json');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
   
     element.style.display = 'none';
     document.body.appendChild(element);
@@ -15,4 +10,15 @@ function exportVincelingMem() {
     element.click();
   
     document.body.removeChild(element);
+}
+
+
+function exportVincelingMem() {
+    let data = compiler.export();
+    if (data === undefined || data.length == 0) {
+        alert("Export failed")
+        return;
+    }
+
+    download("data.json", JSON.stringify(data));
 }
