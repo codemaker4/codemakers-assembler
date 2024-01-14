e34565215c7491fdc1cc9227ca809e5b98476e27
CodeMaker4 <codemaker4@gmail.com>
2024-01-14 15:04:40 +0100

    add niknal emulator export

--- exporters.js
+++ exporters.js
@@ -22,3 +22,33 @@ function exportVincelingMem() {
     download("data.json", JSON.stringify(data));
     postMessage(`Export successfull! Exported ${data.length} bytes.\nPlease put this file in steam/steamapps/workshop/content/387990/2817316401/data.json.`)
 }
+
+function exportNiknalEmulator() {
+    let data = compiler.export();
+    if (data === undefined || data.length == 0) {
+        return;
+    }
+
+    let memory = [];
+    for (let i = 0; i < data.length; i++) {
+        const byte = data[i];
+        while (memory.length <= byte[0]) {
+            memory.push("00000000");
+        }
+
+        let binary = byte[1].toString(2);
+        while (binary.length < 8) {
+            binary = "0" + binary;
+        }
+        
+        memory[byte[0]] = binary;
+    }
+
+    let file = "";
+    for (let i = 0; i < memory.length; i++) {
+        file += memory[i] + "\n";
+    }
+
+    download("memory.txt", file);
+    postMessage(`Export successfull! Exported ${data.length} bytes to an address space of ${binary.length} bytes.\nDownload the emulator from https://github.com/niknal357/SMPU-emulator and replace the memory.txt file.`);
+}
